48e492118043f873a17df11a05cc7813adc53f39
HMC Software Solutions <101631590+hmcorpltd@users.noreply.github.com>
2022-10-19 11:29:36 +0530

    Update messages.ts

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -781,7 +781,7 @@ const generateContextInfo = () => {
 export const patchMessageForMdIfRequired = (message: proto.IMessage) => {
 	const requiresPatch = !!(
 		message.buttonsMessage
-		// || message.templateMessage
+		|| message.templateMessage
 		|| message.listMessage
 	)
 	if(requiresPatch) {
